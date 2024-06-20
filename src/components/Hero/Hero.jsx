51dcb24430930c411772b841d8d197c482d96252
KiptooMannu <Kkgg7241@gmail.com>
2024-06-20 12:27:27 +0300

    changes on blogr

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,34 +1,38 @@
 import React from 'react';
 import './hero.scss';
 import logo from '../../images/logo.svg';
-import wallpaper from '../../images/bg-pattern-intro-desktop.svg';
+import iconHamburger from '../../images/icon-hamburger.svg';
+
 
 const Hero = () => {
   return (
-    <section className="hero-section">
-      <header className="header">
-        <div className="logo"><img src={logo} alt="Blogr Logo" /></div>
-        <nav>
-          <ul>
-            <li>Product</li>
-            <li>Company</li>
-            <li>Connect</li>
-          </ul>
-        </nav>
-        <div className="auth-buttons">
-          <button className="login">Login</button>
-          <button className="signup">Sign Up</button>
+    <header className="hero">
+      <nav className="navbar">
+        <div className="navbar-brand">
+          <img src={logo} alt="Blogr Logo" />
         </div>
-      </header>
+        <div className="navbar-menu">
+          <ul className="navbar-links">
+            <li><a href="#">Product</a></li>
+            <li><a href="#">Company</a></li>
+            <li><a href="#">Connect</a></li>
+          </ul>
+          <div className="navbar-icons">
+            <button className="login-btn">Login</button>
+            <button className="signup-btn">Sign Up</button>
+          </div>
+        </div>
+        <img src={iconHamburger} alt="Menu Icon" className="menu-icon" />
+      </nav>
       <div className="hero-content">
         <h1>A modern publishing platform</h1>
         <p>Grow your audience and build your online brand</p>
-        <div className="cta-buttons">
-          <button className="start">Start for Free</button>
-          <button className="learn">Learn More</button>
+        <div className="hero-buttons">
+          <button className="start-btn">Start for Free</button>
+          <button className="learn-btn">Learn More</button>
         </div>
       </div>
-    </section>
+    </header>
   );
 };
 
